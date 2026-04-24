db8cdb83f1d297a7fe5d4ccf14b42d9f3b79a0db
Abdubakiyev <abdubakiyevabdulloh798@gmail.com>
2026-04-24 13:52:01 +0500

    first commit

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import api from '../lib/axios';
 
-export const API_URL = import.meta.env.VITE_API_URL || 'http://16.16.213.165:5000';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://dokonect-server.onrender.com';
 
 export default api;
 
